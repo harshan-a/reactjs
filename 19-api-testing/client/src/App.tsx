1c7127d60481675b506700e57440ac85ca5707e8
harshan-a <harshan2412005@gmail.com>
2026-02-10 15:29:16 +0530

    initial commit on this branch

--- 19-api-testing/client/src/App.tsx
+++ 19-api-testing/client/src/App.tsx
@@ -21,9 +21,10 @@ import CreateCollection from "./components/CreateCollection"
 import ShowMessage from "./components/ShowMessage"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [collections, setCollections] = useState<CollectionType[]>([])
   const [selectedRequest, setSelectedRequest] = useState<RequestType | null>(
-    null
+    null,
   )
   const [deletionData, setDeletionData] = useState<
     Required<RequestType> | Required<CollectionType> | null
@@ -46,9 +47,13 @@ function App() {
   }
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    loadCollections()
-  }, [])
+    if (!isLoggedIn) {
+      window.location.href = `${import.meta.env.VITE_AUTH_URL}?redirect_url=${import.meta.env.VITE_AUTH_REDIRECT_URL}`
+    } else {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      loadCollections()
+    }
+  }, [isLoggedIn])
 
   useEffect(() => {
     const timeId = showMessage
@@ -59,50 +64,52 @@ function App() {
     return () => clearTimeout(timeId)
   }, [showMessage])
 
-  return (
-    <>
-      <Header />
-      <main>
-        <SetDeletionDataContext.Provider value={setDeletionData}>
-          <SetShowMessageContext.Provider value={setShowMessage}>
-            <SetAllowToSaveContext.Provider value={setAllowToSave}>
-              <AllowToSaveContext.Provider value={allowToSave}>
-                <LoadCollectionsContext.Provider value={loadCollections}>
-                  <CollectionSection
-                    collections={collections}
-                    setSelectedRequest={setSelectedRequest}
-                    setShowCreateCollection={setShowCreateCollection}
-                  />
-
-                  <RequestSection
-                    selectedRequest={selectedRequest}
-                    setSelectedRequest={setSelectedRequest}
-                    collections={collections}
-                  />
-
-                  {deletionData && (
-                    <DeleteComponent
-                      deletionData={deletionData}
-                      setDeletionData={setDeletionData}
-                      loadCollections={loadCollections}
+  if (isLoggedIn) {
+    return (
+      <>
+        <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <main>
+          <SetDeletionDataContext.Provider value={setDeletionData}>
+            <SetShowMessageContext.Provider value={setShowMessage}>
+              <SetAllowToSaveContext.Provider value={setAllowToSave}>
+                <AllowToSaveContext.Provider value={allowToSave}>
+                  <LoadCollectionsContext.Provider value={loadCollections}>
+                    <CollectionSection
+                      collections={collections}
                       setSelectedRequest={setSelectedRequest}
-                    />
-                  )}
-                  {showCreateCollection && (
-                    <CreateCollection
                       setShowCreateCollection={setShowCreateCollection}
-                      loadCollections={loadCollections}
                     />
-                  )}
-                  {showMessage && <ShowMessage message={showMessage} />}
-                </LoadCollectionsContext.Provider>
-              </AllowToSaveContext.Provider>
-            </SetAllowToSaveContext.Provider>
-          </SetShowMessageContext.Provider>
-        </SetDeletionDataContext.Provider>
-      </main>
-    </>
-  )
+
+                    <RequestSection
+                      selectedRequest={selectedRequest}
+                      setSelectedRequest={setSelectedRequest}
+                      collections={collections}
+                    />
+
+                    {deletionData && (
+                      <DeleteComponent
+                        deletionData={deletionData}
+                        setDeletionData={setDeletionData}
+                        loadCollections={loadCollections}
+                        setSelectedRequest={setSelectedRequest}
+                      />
+                    )}
+                    {showCreateCollection && (
+                      <CreateCollection
+                        setShowCreateCollection={setShowCreateCollection}
+                        loadCollections={loadCollections}
+                      />
+                    )}
+                    {showMessage && <ShowMessage message={showMessage} />}
+                  </LoadCollectionsContext.Provider>
+                </AllowToSaveContext.Provider>
+              </SetAllowToSaveContext.Provider>
+            </SetShowMessageContext.Provider>
+          </SetDeletionDataContext.Provider>
+        </main>
+      </>
+    )
+  } else return <>Loading...</>
 }
 
 export default App
--- 19-api-testing/client/src/components/Header.tsx
+++ 19-api-testing/client/src/components/Header.tsx
@@ -1,16 +1,30 @@
 // import logo from "../assets/logo-new.svg"
+import type { Dispatch, SetStateAction } from "react"
 import mobileLogo from "../assets/mobile-logo-new.svg"
 
-import "./Header.css"
+type HeaderProps = {
+  isLoggedIn: boolean
+  setIsLoggedIn: Dispatch<SetStateAction<boolean>>
+}
 
-export default function Header() {
+export default function Header({ isLoggedIn, setIsLoggedIn }: HeaderProps) {
   return (
-    <header className="header">
-      <a href="#">
-        <img className="mobile-logo" src={mobileLogo} alt="harshan-logo" />
+    <header className="fixed top-0 w-full min-w-150 h-15 shadow-primary flex items-center z-100">
+      <a href="#" className="w-fit h-[inherit] text-decoration-none absolute">
+        <img
+          className="mobile-logo py-1.75 px-2.5 box-border h-[inherit]"
+          src={mobileLogo}
+          alt="harshan-logo"
+        />
         {/* <img className="logo" src={logo} alt="harshan-logo" /> */}
       </a>
-      <h4></h4>
+      {isLoggedIn && (
+        <span
+          className="ml-auto mr-5 text-cyan-400 underline underline-offset-4 transition-all hover:text-cyan-500 active:scale-85 cursor-pointer select-none"
+          onClick={() => setIsLoggedIn(false)}>
+          Logout
+        </span>
+      )}
     </header>
   )
 }
